98ae43ef54ead0a34d711a0d56ab9700295be019
HaoranGao0477 <157331955+HaoranGao0477@users.noreply.github.com>
2024-10-08 03:04:04 -0400

    Add files via upload

---
+++ habitat/habitat_manger.py
@@ -0,0 +1,41 @@
+from typing import Optional, List
+
+from wildlife_tracker.animal_management.animal import Animal
+from wildlife_tracker.habitat_management.habitat import Habitat
+from wildlife_tracker.migration_management.migration import Migration
+from wildlife_tracker.migration_management.migration_path import MigrationPath
+
+class HabitatManager:
+
+    def assign_animals_to_habitat(self, habitat_id: int, animals: List[Animal]) -> None:
+        pass
+
+    def create_habitat(self, habitat_id: int, geographic_area: str, size: int, environment_type: str) -> Habitat:
+        pass
+
+    def get_animals_in_habitat(self, habitat_id: int) -> List[Animal]:
+        pass
+
+    def get_habitat_by_id(self, habitat_id: int) -> Habitat:
+        pass
+
+    def get_habitat_details(self, habitat_id: int) -> dict:
+        pass
+
+    def get_habitats_by_geographic_area(self, geographic_area: str) -> List[Habitat]:
+        pass
+
+    def get_habitats_by_size(self, size: int) -> List[Habitat]:
+        pass
+
+    def get_habitats_by_type(self, environment_type: str) -> List[Habitat]:
+        pass
+
+    def remove_habitat(self, habitat_id: int) -> None:
+        pass
+
+    def update_habitat_details(self, habitat_id: int, **kwargs: dict[str, Any]) -> None:
+        pass
+
+
+
